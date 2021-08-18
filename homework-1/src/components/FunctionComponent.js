1f8f8c3d8b72f4afc248868f567bb658d02fef2c
Sairaj Bhise <sairajbhise98@gmail.com>
2021-08-18 10:50:19 +0530

    Updated

--- homework-1/src/components/FunctionComponent.js
+++ homework-1/src/components/FunctionComponent.js
@@ -1,9 +1,15 @@
 let FunctionComponent = (props) => {
     return (
+         <>
         <div>
             <h1>Function Component</h1>
             <input type='text' onKeyUp={(e)=>{props.show(e.target.value)}}/>
+            
         </div>
+        <div>
+            <h1>Hello</h1>
+        </div>
+        </>
     );
 }
 
